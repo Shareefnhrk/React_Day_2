7802464871da707641f6ed507a0d8921b5a42890
unknown <cogjava1781@iiht.tech>
2022-07-23 14:30:43 +0530

    redux-demo-experiment

--- day-14/redux-demo/src/Counter.js
+++ day-14/redux-demo/src/Counter.js
@@ -2,45 +2,57 @@ import { createStore } from "redux";
 
 const INCREMENT = 'INCREMENT';  //action types
 const DECREMENT = 'DECREMENT';
+const ADD_TODO = "ADD_TODO";
 
 
 function increment() {
-  return { type: INCREMENT };  //action creators
+    return { 
+        type: ADD_TODO,  //action
+    payload:{text:"learn Redux"} 
+};  //action creators
 }
 function decrement() {
-  return { type: DECREMENT };
+    return { type: DECREMENT };
 }
 
 
-function reducer(state = 0, action) {   //reducer
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
-    default:
-      return state;
-  }
+function reducer(state = {counrte:5}, action) {   //reducer
+    switch (action.type) {
+        case ADD_TODO:
+            return{...state,dataformComponent:action.payload}
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
 }
 
 
 var store = createStore(reducer);  //redux-store
-
-export default function LogState() {
-  console.log(store.getState().toString());
-  return(
-    <div>
-    <button>click</button>
-    </div>
-  )
+function enableDevTools() {
+    return (
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 }
 
-store.subscribe(LogState);
+export default function LogState() {
+    // console.log(store.getState().toString());
+    return (
+        <div>
+            <button onClick={(event) => store.dispatch(increment())}>increment</button>
+            <button onClick={() => store.dispatch(decrement())}>decrement</button>
+        </div>
+    )
+}
+
+store.subscribe(LogState);  //register with reduxstore
 
 store.dispatch({ type: '' });
 store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
