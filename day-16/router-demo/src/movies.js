c08da050c6a581f555fd72658ad4b3a4a93fce7e
unknown <cogjava1781@iiht.tech>
2022-07-26 09:38:17 +0530

    Router-demo

---
+++ day-16/router-demo/src/movies.js
@@ -0,0 +1,29 @@
+import {Link,useParams} from "react-router-dom";
+import  Movies from "./App";
+
+export  function MovieList({ movies }) {
+    return (
+      <div>
+        <h2>Movies</h2>
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`./${movie.id}`}>{movie.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  export function MovieDetail() {
+    let { id } = useParams();
+    id = Number(id);
+    const movie = Movies.find((movie) => movie.id === id);
+  
+    return (
+      <div>
+        <h3>{movie.name}</h3>
+        <p>{movie.description}</p>
+      </div>
+    );
+  }
